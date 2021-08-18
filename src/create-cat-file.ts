b21acba86f406e9cbd2b369c6811ce6bfa825772
Nikita Glukhi <nikita.glukhi@valor-software.com>
2021-08-18 13:08:30 +0300

    refactor(core): rewrote to Typescript  (#7)
    * add new icon
    
    * refactor(core): rewrited to Typescript

---
+++ src/create-cat-file.ts
@@ -0,0 +1,12 @@
+import { execSync } from "child_process";
+
+export function createCatFile(email: string, api_key: string): string {
+  return `cat >~/.netrc <<EOF
+machine api.heroku.com
+    login ${email}
+    password ${api_key}
+machine git.heroku.com
+    login ${email}
+    password ${api_key}
+EOF`;
+}
